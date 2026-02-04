bacd8814de615675eef34eb8e6666f027a66e227
b01tech <bruno01.tech@gmail.com>
2026-02-04 16:18:25 -0300

    feat(usuario): encrypt password before storing user

--- backend/src/API/Handlers/Usuario/CreateUsuarioUseCase.cs
+++ backend/src/API/Handlers/Usuario/CreateUsuarioUseCase.cs
@@ -1,17 +1,19 @@
 ﻿using Core.Dtos;
 using Core.Repositories;
+using Core.Services;
 using Core.UseCases.Usuario;
 
 namespace API.Handlers.Usuario;
 
-internal class CreateUsuarioUseCase(IUsuarioRepository repository) : ICreateUsuarioUseCase
+internal class CreateUsuarioUseCase(IUsuarioRepository repository, IEncrypter encrypter) : ICreateUsuarioUseCase
 {
     public async Task ExecuteAsync(CreateUsuarioDto request)
     {
+        var hash = encrypter.Encrypt(request.Senha);
         var usuario = new Core.Entities.Usuario(
             request.Nome,
             request.Email,
-            request.Senha
+            hash
         );
         await repository.AddAsync(usuario);
     }
