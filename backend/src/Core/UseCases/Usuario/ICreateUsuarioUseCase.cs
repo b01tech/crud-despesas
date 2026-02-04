09e3f53e9efcc07d689093d52d96216bc13c793f
b01tech <bruno01.tech@gmail.com>
2026-02-04 16:02:09 -0300

    feat(usuario): add use case interfaces for login and create operations

---
+++ backend/src/Core/UseCases/Usuario/ICreateUsuarioUseCase.cs
@@ -0,0 +1,8 @@
+﻿using Core.Dtos;
+
+namespace Core.UseCases.Usuario;
+
+public interface ICreateUsuarioUseCase
+{
+    Task ExecuteAsync(CreateUsuarioDto request);
+}
---
+++ backend/src/Core/UseCases/Usuario/ILoginUsuarioUseCase.cs
@@ -0,0 +1,8 @@
+﻿using Core.Dtos;
+
+namespace Core.UseCases.Usuario;
+
+public interface ILoginUsuarioUseCase
+{
+    Task ExecuteAsync(LoginUsuarioDto request);
+}
