de73401749c65f6cd6de6c463aa14e5459b705d6
b01tech <bruno01.tech@gmail.com>
2026-02-04 09:46:26 -0300

    refactor(app): replace static title with header

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './shared/header/header';
+import { Despesas } from './despesas/despesas';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Despesas],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
