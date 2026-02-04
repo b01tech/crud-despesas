e12f3d1e7552ef235e0d72024cb43e0c1a4bf01e
b01tech <bruno01.tech@gmail.com>
2026-02-03 22:41:38 -0300

    feat(infra): add AppDbContext

---
+++ backend/src/Infra/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Despesa> Despesas { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Despesa>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Descricao).IsRequired();
+            entity.Property(e => e.DataHora).IsRequired();
+            entity.Property(e => e.Valor).IsRequired();
+            entity.Property(e => e.Pago).IsRequired();
+        });
+    }
+}
