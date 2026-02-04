e9653bf543ab68672e40b73a8aa7ed5f747e1133
b01tech <bruno01.tech@gmail.com>
2026-02-03 22:44:38 -0300

    feat: configure service and endpoint mappings

--- backend/src/API/Program.cs
+++ backend/src/API/Program.cs
@@ -1,7 +1,16 @@
+using API.Handlers;
+using API.Controllers;
+using Infra.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddInfrastructure(builder.Configuration)
+    .AddUseCases();
 
 var app = builder.Build();
 
+app.MapAppEndpoints();
 app.UseHttpsRedirection();
 
 app.Run();
