bc29e3163bfa891b21a767ff2a0216d3455f2265
b01tech <bruno01.tech@gmail.com>
2026-02-03 22:43:17 -0300

    feat(infra): add infrastructure extensions for dependency injection

---
+++ backend/src/Infra/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Core.Repositories;
+using Infra.Data;
+using Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Extensions;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("ConnectionString") ?? string.Empty;
+        services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
+        services.AddScoped<IDespesaRepository, DespesasRepository>();
+
+        return services;
+    }
+}
