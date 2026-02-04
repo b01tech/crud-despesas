7683892591ad742f3fa8d35df74721c6c25c042c
b01tech <bruno01.tech@gmail.com>
2026-02-04 14:26:28 -0300

    feat(despesas): connect create expense form to backend service

--- frontend/src/app/despesas/criar-despesa/criar-despesa.ts
+++ frontend/src/app/despesas/criar-despesa/criar-despesa.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { DepesaCreateRequest } from '../models/depesa-create-request';
+import { DepesaService } from '../services/depesa-service';
 
 @Component({
   selector: 'app-criar-despesa',
@@ -10,6 +12,8 @@ import { RouterLink } from '@angular/router';
 })
 export class CriarDespesa {
   private readonly _fb = inject(FormBuilder);
+  private readonly _despesaService = inject(DepesaService);
+  private readonly _router = inject(Router);
 
   form = this._fb.group({
     descricao: ['', [Validators.required]],
@@ -17,4 +21,21 @@ export class CriarDespesa {
     valor: [0, [Validators.required, Validators.min(0.01)]],
     pago: [false],
   });
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+    const request: DepesaCreateRequest = this.form.value as DepesaCreateRequest;
+
+    this._despesaService.postDespesa(request).subscribe({
+      next: (response) => {
+        this.form.reset();
+        this._router.navigate(['/despesas']);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
