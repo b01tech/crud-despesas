0f76a4c3f96d78dea7239be9cc29a6cd4051247b
b01tech <bruno01.tech@gmail.com>
2026-02-04 11:12:52 -0300

    feat(despesas): implement expense table component

--- frontend/src/app/despesas/tabela/tabela.ts
+++ frontend/src/app/despesas/tabela/tabela.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { DespesasApiResponse } from '../models/despesas-api-response';
+import { DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tabela',
-  imports: [],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './tabela.html',
   styleUrl: './tabela.css',
 })
 export class Tabela {
-
+  listaDespesas = input<DespesasApiResponse[]>([]);
 }
