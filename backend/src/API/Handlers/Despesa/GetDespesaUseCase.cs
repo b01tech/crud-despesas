ea3613aed3c45f1c02795d6193a7f4fdd23dc720
b01tech <bruno01.tech@gmail.com>
2026-02-04 08:38:27 -0300

    feat(despesa): implement GetDespesaUseCase

--- backend/src/API/Handlers/Despesa/GetDespesaUseCase.cs
+++ backend/src/API/Handlers/Despesa/GetDespesaUseCase.cs
@@ -1,8 +1,16 @@
-﻿using Core.UseCases.Despesa;
+﻿using Core.Dtos;
+using Core.Repositories;
+using Core.UseCases.Despesa;
 
 namespace API.Handlers.Despesa;
 
-public class GetDespesaUseCase : IGetDespesaUseCase
+public class GetDespesaUseCase(IDespesaRepository repository) : IGetDespesaUseCase
 {
-
+    public async Task<DespesaDto?> ExecuteAsync(Guid id)
+    {
+        var despesa = await repository.GetByIdAsync(id);
+        return despesa is null
+            ? null
+            : new DespesaDto(despesa.Descricao, despesa.DataHora, despesa.Pago, despesa.Valor);
+    }
 }
