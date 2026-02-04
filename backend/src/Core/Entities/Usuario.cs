474f25bf9461493b5fd332fecbe09226f3454622
b01tech <bruno01.tech@gmail.com>
2026-02-04 16:00:38 -0300

    feat: add Usuario entity

---
+++ backend/src/Core/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Entities;
+
+public class Usuario
+{
+    public Guid Id { get; init; } = Guid.CreateVersion7();
+    public string Nome { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public string HashSenha { get; private set; } = string.Empty;
+
+    protected Usuario() { }
+
+    public Usuario(string nome, string email, string hashSenha)
+    {
+        Nome = nome;
+        Email = email;
+        HashSenha = hashSenha;
+    }
+}
