a62ccdae5e81380bee6a87dc0cd24d8c455293d5
b01tech <bruno01.tech@gmail.com>
2026-02-04 15:16:10 -0300

    feat: add total expenses calculation

--- frontend/src/app/despesas/tabela/tabela.ts
+++ frontend/src/app/despesas/tabela/tabela.ts
@@ -1,9 +1,8 @@
-import { Component, inject, input, output } from '@angular/core';
-import { DespesasApiResponse } from '../models/despesas-api-response';
-import { DatePipe, CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, computed, inject, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DespesasApiResponse } from '../models/despesas-api-response';
 import { DepesaService } from '../services/depesa-service';
-import { DespesaUpdateRequest } from '../models/despesa-update-request';
 
 @Component({
   selector: 'app-tabela',
@@ -15,6 +14,11 @@ export class Tabela {
   private readonly _despesaService = inject(DepesaService);
   listaDespesas = input<DespesasApiResponse[]>([]);
   despesaAtualizada = output<void>();
+  totalDespesas = computed(() =>
+    this.listaDespesas()
+      .filter((d) => d.pago === false)
+      .reduce((acc, cur) => acc + cur.valor, 0),
+  );
 
   deleteDespesa(id: string) {
     this._despesaService.deleteDespesa(id).subscribe({
