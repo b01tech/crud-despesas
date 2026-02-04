aa25f3a31b5573939ee82152a5677b6be4012518
b01tech <bruno01.tech@gmail.com>
2026-02-04 08:39:03 -0300

    refactor(Despesa): implement remove use case

--- backend/src/API/Handlers/Despesa/RemoveDespesaUseCase.cs
+++ backend/src/API/Handlers/Despesa/RemoveDespesaUseCase.cs
@@ -1,8 +1,15 @@
-﻿using Core.UseCases.Despesa;
+﻿using Core.Repositories;
+using Core.UseCases.Despesa;
 
 namespace API.Handlers.Despesa;
 
-public class RemoveDespesaUseCase : IRemoveDespesaUseCase
+public class RemoveDespesaUseCase(IDespesaRepository repository) : IRemoveDespesaUseCase
 {
+    public async Task ExecuteAsync(Guid id)
+    {
+        var despesa = await repository.GetByIdAsync(id);
+        if (despesa is null) return;
 
+        await repository.DeleteAsync(despesa);
+    }
 }
