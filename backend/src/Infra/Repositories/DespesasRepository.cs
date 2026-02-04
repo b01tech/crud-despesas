edec83b65823bfc270aede14b45c239215002145
b01tech <bruno01.tech@gmail.com>
2026-02-03 22:42:42 -0300

    feat(infra): add initial despesas repository implementation

---
+++ backend/src/Infra/Repositories/DespesasRepository.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Core.Repositories;
+using Infra.Data;
+
+namespace Infra.Repositories;
+
+internal class DespesasRepository(AppDbContext dbContext) : IDespesaRepository
+{
+    public Task<Despesa?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Despesa>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async  Task AddAsync(Despesa despesa)
+    {
+        await dbContext.Despesas.AddAsync(despesa);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public Task UpdateAsync(Despesa despesa)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Despesa despesa)
+    {
+        throw new NotImplementedException();
+    }
+}
