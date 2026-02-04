c4a6565d8da3dc71e58050b1b08f6136514a798c
b01tech <bruno01.tech@gmail.com>
2026-02-04 11:12:37 -0300

    feat(despesas): fetch and display expenses from service

--- frontend/src/app/despesas/despesas.ts
+++ frontend/src/app/despesas/despesas.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Tabela } from './tabela/tabela';
+import { DepesaService } from './services/depesa-service';
+import { DespesasApiResponse } from './models/despesas-api-response';
 
 @Component({
   selector: 'app-despesas',
@@ -7,4 +9,20 @@ import { Tabela } from './tabela/tabela';
   templateUrl: './despesas.html',
   styleUrl: './despesas.css',
 })
-export class Despesas {}
+export class Despesas implements OnInit {
+  private readonly _despesaService = inject(DepesaService);
+
+  despesas = signal<DespesasApiResponse[]>([]);
+
+  ngOnInit() {
+    this._despesaService.getDespesas().subscribe({
+      next: (response) => {
+        console.log('Dados recebidos:', response);
+        this.despesas.set(response);
+      },
+      error: (error) => {
+        console.error('Erro ao buscar despesas:', error);
+      },
+    });
+  }
+}
