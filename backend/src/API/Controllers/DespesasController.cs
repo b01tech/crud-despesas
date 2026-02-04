01212fa4f5e490c618e771debde72509648d645a
b01tech <bruno01.tech@gmail.com>
2026-02-04 08:39:12 -0300

    feat(controller): add CRUD endpoints for Despesas

--- backend/src/API/Controllers/DespesasController.cs
+++ backend/src/API/Controllers/DespesasController.cs
@@ -17,5 +17,26 @@ public static class DespesasController
         }).WithName("CreateDespesa")
         .WithSummary("Cria uma nova despesa")
         .Produces(StatusCodes.Status201Created);
+
+        group.MapGet("/", async ([FromServices]IGetAllDespesaUseCase useCase) =>
+        {
+            var despesas = await useCase.ExecuteAsync();
+            return Results.Ok(despesas);
+        });
+        group.MapGet("/{id:guid}", async ([FromRoute]Guid id, [FromServices]IGetDespesaUseCase useCase) =>
+        {
+            var despesa = await useCase.ExecuteAsync(id);
+            return Results.Ok(despesa);
+        });
+        group.MapDelete("/{id:guid}", async ([FromRoute]Guid id, [FromServices]IRemoveDespesaUseCase useCase) =>
+        {
+            await useCase.ExecuteAsync(id);
+            return Results.NoContent();
+        });
+        group.MapPut("/", async ([FromRoute]Guid id, [FromBody]UpdateDespesaDto request, [FromServices]IUpdateDespesaUseCase useCase) =>
+        {
+            await useCase.ExecuteAsync(request);
+            return Results.NoContent();
+        });
     }
 }
