1bec75719d748d9e241eafa9dc5b28d7e0fc61e3
b01tech <bruno01.tech@gmail.com>
2026-02-04 13:25:34 -0300

    style(frontend): update global and component styles

--- frontend/src/app/despesas/criar-despesa/criar-despesa.ts
+++ frontend/src/app/despesas/criar-despesa/criar-despesa.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-despesa',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './criar-despesa.html',
   styleUrl: './criar-despesa.css',
 })
 export class CriarDespesa {
+  private readonly _fb = inject(FormBuilder);
 
+  form = this._fb.group({
+    descricao: ['', [Validators.required]],
+    dataHora: ['', [Validators.required]],
+    valor: [0, [Validators.required, Validators.min(0.01)]],
+    pago: [false],
+  });
 }
