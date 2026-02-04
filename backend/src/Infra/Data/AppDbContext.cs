3b3330c71e3462028b6084cb17307beb170e7ef0
b01tech <bruno01.tech@gmail.com>
2026-02-04 16:00:56 -0300

    feat(Data): add Usuario DbSet

--- backend/src/Infra/Data/AppDbContext.cs
+++ backend/src/Infra/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace Infra.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Despesa> Despesas { get; set; } = null!;
+    public DbSet<Usuario> Usuarios { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -16,6 +17,14 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
             entity.Property(e => e.DataHora).IsRequired();
             entity.Property(e => e.Valor).IsRequired();
             entity.Property(e => e.Pago).IsRequired();
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Nome).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.HashSenha).IsRequired();
         });
     }
 }
