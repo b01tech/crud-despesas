432d5073d8e18a5807217e44fc42d3dda3bb0759
b01tech <bruno01.tech@gmail.com>
2026-02-03 22:41:18 -0300

    feat: add Despesa entity

---
+++ backend/src/Core/Entities/Despesa.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Entities;
+
+public class Despesa
+{
+    public Guid Id { get; init; } = Guid.CreateVersion7();
+    public string Descricao { get; private set; } = string.Empty;
+    public DateTime DataHora { get; private set; }
+    public decimal Valor { get; private set; }
+    public bool Pago { get; private set; } = false;
+
+    protected Despesa() { }
+
+    public Despesa(string descricao, DateTime dataHora, decimal valor, bool pago = false)
+    {
+        Descricao = descricao;
+        DataHora = dataHora;
+        Valor = valor;
+        Pago = pago;
+    }
+}
