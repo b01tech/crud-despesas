32e1e38b7044480207662fbe7bbfccc7ddc72ab2
b01tech <bruno01.tech@gmail.com>
2026-02-03 22:44:09 -0300

    feat: add endpoint for creating expenses

---
+++ backend/src/API/Controllers/DespesasController.cs
@@ -0,0 +1,21 @@
+﻿using Core.Dtos;
+using Core.UseCases.Despesa;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public static class DespesasController
+{
+    public static void MapAppEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("despesas").WithTags("Despesas");
+
+        group.MapPost("/", async ([FromBody]CreateDespesaDto request, [FromServices]ICreateDespesaUseCase useCase) =>
+        {
+            await useCase.ExecuteAsync(request);
+            return Results.Created("","criado com sucesso");
+        }).WithName("CreateDespesa")
+        .WithSummary("Cria uma nova despesa")
+        .Produces(StatusCodes.Status201Created);
+    }
+}
