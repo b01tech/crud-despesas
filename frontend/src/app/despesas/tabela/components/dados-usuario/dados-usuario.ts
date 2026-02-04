c1ddef7b2245f0faaa8b5d6f2825c7e4d4fed781
b01tech <bruno01.tech@gmail.com>
2026-02-04 15:27:25 -0300

    feat(despesas): add user data component with "Add Expense" button

---
+++ frontend/src/app/despesas/tabela/components/dados-usuario/dados-usuario.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dados-usuario',
+  imports: [RouterLink],
+  templateUrl: './dados-usuario.html',
+  styleUrl: './dados-usuario.css',
+})
+export class DadosUsuario {}
