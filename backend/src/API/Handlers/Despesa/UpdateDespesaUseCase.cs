fc7861fa40dac1e99aea97c0b34cf49d8d43eb58
b01tech <bruno01.tech@gmail.com>
2026-02-04 08:38:51 -0300

    refactor(Despesa): implement UpdateDespesaUseCase

--- backend/src/API/Handlers/Despesa/UpdateDespesaUseCase.cs
+++ backend/src/API/Handlers/Despesa/UpdateDespesaUseCase.cs
@@ -1,8 +1,16 @@
-﻿using Core.UseCases.Despesa;
+﻿using Core.Dtos;
+using Core.Repositories;
+using Core.UseCases.Despesa;
 
 namespace API.Handlers.Despesa;
 
-public class UpdateDespesaUseCase : IUpdateDespesaUseCase
+public class UpdateDespesaUseCase(IDespesaRepository repository) : IUpdateDespesaUseCase
 {
-
+    public async Task ExecuteAsync(UpdateDespesaDto request)
+    {
+        var despesa = await repository.GetByIdAsync(request.Id);
+        if (despesa is null) return;
+        despesa.Update(request.Descricao, request.DataHora, request.Valor, request.Pago);
+        await repository.UpdateAsync(despesa);
+    }
 }
