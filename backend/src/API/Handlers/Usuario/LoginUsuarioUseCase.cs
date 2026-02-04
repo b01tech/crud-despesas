7fc99fb96d4f6aaacd6e798e18148c84dd84aa53
b01tech <bruno01.tech@gmail.com>
2026-02-04 16:17:03 -0300

    feat(usuario): return token response on login

--- backend/src/API/Handlers/Usuario/LoginUsuarioUseCase.cs
+++ backend/src/API/Handlers/Usuario/LoginUsuarioUseCase.cs
@@ -1,17 +1,21 @@
 ﻿using Core.Dtos;
 using Core.Repositories;
+using Core.Services;
 using Core.UseCases.Usuario;
 
 namespace API.Handlers.Usuario;
 
-public class LoginUsuarioUseCase(IUsuarioRepository repository): ILoginUsuarioUseCase
+public class LoginUsuarioUseCase(IUsuarioRepository repository, IEncrypter encrypter) : ILoginUsuarioUseCase
 {
-    public async Task ExecuteAsync(LoginUsuarioDto request)
+    public async Task<TokenResponseDto?> ExecuteAsync(LoginUsuarioDto request)
     {
         var usuario = await repository.GetByEmailAsync(request.Email);
         if (usuario is null)
-        {
-            throw new ApplicationException("Email ou senha inválidos");
-        }
+            return null;
+        var isPasswordValid = encrypter.Verify(request.Senha, usuario.HashSenha);
+        if (!isPasswordValid)
+            return null;
+
+        return new TokenResponseDto("token criado com sucesso");
     }
 }
--- backend/src/Core/UseCases/Usuario/ILoginUsuarioUseCase.cs
+++ backend/src/Core/UseCases/Usuario/ILoginUsuarioUseCase.cs
@@ -4,5 +4,5 @@ namespace Core.UseCases.Usuario;
 
 public interface ILoginUsuarioUseCase
 {
-    Task ExecuteAsync(LoginUsuarioDto request);
+    Task<TokenResponseDto?> ExecuteAsync(LoginUsuarioDto request);
 }
