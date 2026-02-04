2f0aa840a483bac8921de111a947b61a392d65e9
b01tech <bruno01.tech@gmail.com>
2026-02-03 22:43:50 -0300

    feat(api): add dependency injection setup for despesa use cases

---
+++ backend/src/API/Handlers/HandlersExtensions.cs
@@ -0,0 +1,17 @@
+﻿using API.Handlers.Despesa;
+using Core.UseCases.Despesa;
+
+namespace API.Handlers;
+
+public static class HandlersExtensions
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddScoped<ICreateDespesaUseCase, CreateDespesaUseCase>();
+        services.AddScoped<IUpdateDespesaUseCase, UpdateDespesaUseCase>();
+        services.AddScoped<IRemoveDespesaUseCase, RemoveDespesaUseCase>();
+        services.AddScoped<IGetAllDespesaUseCase, GetAllDespesaUseCase>();
+        services.AddScoped<IGetDespesaUseCase, GetDespesaUseCase>();
+        return services;
+    }
+}
