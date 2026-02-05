c9dd8198eca0464fae52144136e165fee986f884
b01tech <bruno01.tech@gmail.com>
2026-02-05 10:31:37 -0300

    feat(auth): add route guards and redirect to login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,31 +1,38 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/guard/auth.guard';
+import { loginGuard } from './auth/guard/login.guard';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'despesas',
+    redirectTo: 'auth/login',
   },
   {
     path: 'despesas',
+    canActivate: [authGuard],
     loadComponent: () => import('./despesas/despesas').then((mod) => mod.Despesas),
   },
   {
     path: 'despesas/criar',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./despesas/criar-despesa/criar-despesa').then((mod) => mod.CriarDespesa),
   },
   {
     path: 'despesas/editar/:id',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./despesas/editar-despesa/editar-despesa').then((mod) => mod.EditarDespesa),
   },
   {
     path: 'auth/usuario',
+    canActivate: [loginGuard],
     loadComponent: () => import('./auth/usuario/usuario').then((mod) => mod.Usuario),
   },
   {
     path: 'auth/login',
+    canActivate: [loginGuard],
     loadComponent: () => import('./auth/login/login').then((mod) => mod.Login),
   },
 ];
